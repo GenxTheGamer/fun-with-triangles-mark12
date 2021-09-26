cd6b35b084a54e69bcf8c85a254aa8e7b22e2357
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-26 12:35:56 +0530

    ex-01: Structure the html for Is Triangle Problem

---
+++ app.js
@@ -0,0 +1,5 @@
+const inputs = document.querySelectorAll(".angle");
+const isTriangleButton = document.querySelector("#is-triangle");
+const outputArea = document.querySelector("#output");
+
+console.log(inputs);
