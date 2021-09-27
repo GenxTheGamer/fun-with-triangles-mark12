39ffc7d278f93acdc07751686e27b41730dce51a
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-27 18:28:16 +0530

    added nav to all pages.

--- areaOfTriangle.js
+++ areaOfTriangle.js
@@ -1,10 +1,19 @@
 const areaInputs = document.querySelectorAll(".area-input");
 const areaButton = document.querySelector("#area-result-btn");
+const output = document.querySelector("#output");
+
+function getArea(base, height) {
+  const baseHeight = base * height;
+  const area = (1 / 2) * baseHeight;
+  return area;
+}
 
 function calculateArea() {
   const inputA = areaInputs[0].value;
   const inputB = areaInputs[1].value;
-  console.log(inputA, inputB);
+  const areaResult = getArea(inputA, inputB);
+
+  output.innerText = "Area is " + areaResult;
 }
 
 areaButton.addEventListener("click", calculateArea);
